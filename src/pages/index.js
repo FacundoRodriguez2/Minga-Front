358a4a5844b366ce5e99be78bdfb652d98285dfb
Eliana Stefani Guarino <119450786+stefy0075@users.noreply.github.com>
2023-03-06 22:53:24 -0300

    Company and fixsed (#14)
    * form
    
    * Company and fixed
    
    * fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import AuthorForm from "./AuthorForm/authorform.js"
 import { createBrowserRouter } from "react-router-dom";
 import Hero from "./hero/hero";
 import IndexLayout from "../layouts/IndexLayout/IndexLayout";
@@ -7,6 +6,8 @@ import Index from "./index/index";
 import AuthForm from './AuthForm/AuthForm'
 import ChaptherForm from "./ChapterForm/ChapterForm";
 import LogIn from "./LogIn/LogIn";
+import FormAuthor from "./AuthorForm/AuthorForm";
+import FormCompany from "./CompanyForm/companyForm";
 
 const router = createBrowserRouter([
     { 
@@ -24,15 +25,12 @@ const router = createBrowserRouter([
         children: [
             { path: '/register' , element: <AuthForm /> },
             { path: '/signin' , element: <LogIn /> },
-            { path: '/chapther-form/:manga_id' , element: <ChaptherForm /> }
+            { path: '/chapther-form/:manga_id' , element: <ChaptherForm /> },
+            { path: '/author-form'  , element: <FormAuthor/> },
+            { path: '/company-form' , element: <FormCompany/> }
 
         ]
-    },
-    { 
-        path: '/author-form' , 
-        element: <AuthorForm/> 
-    },
-
+    }
 ])
 
 export default router
