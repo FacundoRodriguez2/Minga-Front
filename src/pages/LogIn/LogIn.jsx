abb271069516b7e832956f7cdb2b1838a808ef63
facundo rodriguez <facundo2punto0@gmail.com>
2023-03-07 09:57:36 -0300

    fixes

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LogIn.css'
+import LoginForm from '../../components/LoginForm/LoginForm'
 import RegisterFieldset from '../../components/registerfieldset/registerfieldset'
 import email from '../../images/@.svg'
 import lock from '../../images/lock1.svg'
@@ -9,7 +10,7 @@ import LogInimg from '../../images/LogIn.png'
 import { Link as Anchor } from 'react-router-dom'
 
 
-export default function LogIn() {
+export default function LogIn({renderRegister, handleRender}) {
   return (
     <section className='log-in'>
       <div className='img-section'>
@@ -23,16 +24,11 @@ export default function LogIn() {
         </div>
 
         <div className='form'>
-          <RegisterFieldset legendText='Email' inputType='email' inputName='mail' inputId='mail' imgSrc={email} imgAlt='@' />
-          <RegisterFieldset legendText='Password' inputType='password' inputName='password' inputId='password' imgSrc={lock} imgAlt='lock' />
-          
-          <Input className='sign-up' type='submit' value="Sign up" />
-          <a href='#' className='sign-in-google'> <img src={googleLogo} alt="googleLogo" /><span>Sign in with Google</span></a>
-          <p>Already have an account? <Anchor to='/register' className='link'>Sign up</Anchor></p>
-          <p>Go back to <Anchor to='/' className='link'>home page</Anchor></p>
+          <LoginForm renderRegister={renderRegister} />
         </div>
       </div>
 
     </section>
   )
 }
+
