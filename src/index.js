c5a0fede89c71296af5fefd832d91cb55d2cead2
Eliana <guarinoeliana@gmail.com>
2023-03-28 17:37:14 -0300

    Sign In con Google

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId="630129650247-gkc9jouc4jgrg2sapdes4hucuc4jkp11.apps.googleusercontent.com">
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
+</GoogleOAuthProvider>,
 );
 
 reportWebVitals();
